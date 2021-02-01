5e2742189eb1e20215a8aad7cda6eb56f5a625ed
Anil <anil.nerella4321@gmail.com>
2021-02-01 22:18:14 +0530

    Assignment

---
+++ Java Assignments/JAVA ASSIGNMENT 12.java
@@ -0,0 +1,95 @@
+class Q{
+	int num;
+	boolean value=false;
+	
+	public synchronized void put(int num) 
+	{
+	   while(value) {
+		   try {
+			   wait();
+		   }
+		   catch(Exception e) {
+			   
+		   }
+	   }
+	   System.out.println("Put : "+num);
+	   this.num=num;
+	   value=true;
+	   notify();
+	}
+	
+	public synchronized void get()
+	{
+		while(!value)
+		{
+			try {
+				wait();
+			}
+			catch(Exception e) {
+				
+			}
+		}
+		System.out.println("get : "+num);
+		value=false;
+		notify();
+	}
+	
+}
+
+class Producer implements Runnable
+{
+	Q q;
+	public Producer(Q q)
+	{
+		this.q=q;
+		Thread t=new Thread(this,"Producer");
+		t.start();
+	}
+	public void run() {
+		int i=0;
+		while(true)
+		{
+			q.put(i++);
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+	}
+}
+
+class Consumer implements Runnable
+{
+	Q q;
+	public Consumer(Q q)
+	{
+		this.q=q;
+		Thread t=new Thread(this,"Consumer");
+		t.start();
+	}
+	public void run() {
+		while(true)
+		{
+			q.get();
+			try {
+				Thread.sleep(5000);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+	}
+}
+
+public class ConsumerProducer {
+
+	public static void main(String[] args) {
+		Q q=new Q();
+		new Producer(q);
+		new Consumer(q);
+		
+	}
+
+}
